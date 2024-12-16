d87e3eb60aa8f64d37c3bd37f51559153fc8f7fd
Nedblack <108587461+Nedblack@users.noreply.github.com>
2024-12-16 17:11:37 +0300

    Update oten.js

--- oten.js
+++ oten.js
@@ -1,23 +1,23 @@
 function addCustomStyles() {
   $("style").last().append(`
-  #users .uzr.superUser img.co {
+  #users .uzr.baqer img.co {
     width: 0px !important;
     height: 0px !important;
     margin-right: 1px;
   }
 
-  #users .uzr.superUser span.uhash {
+  #users .uzr.baqer span.uhash {
     -webkit-text-fill-color: transparent;
     width: 0px !important;
     height: 0px !important;
     margin-right: 1px;
   }
 
-  #users .uzr.superUser img.u-ico {
+  #users .uzr.baqer img.u-ico {
     margin-left: 5px;
   }
 
-  #users .uzr.superUser img.ustat {
+  #users .uzr.baqer img.ustat {
     width: 12px !important;
     height: 12px !important;
     min-height: 0% !important;
@@ -27,62 +27,53 @@ function addCustomStyles() {
     margin-left: 0px;
     margin-top: 1px;
     border: 1px solid #e7ebe8;
-    box-shadow: inset 0001 Doo2px #f0c01f;
-    box-shadow: 0 0 1px inset 0 0 0px 0 0 0 0px #000%;
-    rgb(000/ 8%), rgb(0, 0, 0), rgb(0, 0, 0);
+    box-shadow: inset 0 0 2px #f0c01f;
   }
 
-  #users .uzr.superUser .fitimg.u-pic {
-    border: 1px solid  #fff;
-    8px 0px 8px-8px;
-    border-radius:
-    box-shadow: 00 3px
-    inset 0 0 0px
-    0 0 0 0px #000;
+  #users .uzr.baqer .fitimg.u-pic {
+    border: 1px solid #fff;
+    border-radius: 100px;
     margin-top: 7px;
     margin-right: 2px;
-    border-radius: 100px;
     margin-left: -5px;
     height: 52px !important;
-    rgb(255, 171, 0),
-    rgb(0, 0, 0),
     margin-bottom: 10px !important;
     padding: 26px !important;
   }
 
-  #users .uzr.superUser .u-msg {
+  #users .uzr.baqer .u-msg {
     margin-left: 8px;
     margin-top: 2px !important;
     background-size: 100% auto;
     -webkit-background-clip: text;
-    -webkit-text-fill-color: #0000;
+    -webkit-text-fill-color: transparent;
     background-image: linear-gradient(-225deg, #696969 0%, #434343 29%, #f00 67%, #696969 100%);
     padding: 0px !important;
     font-size: 89% !important;
   }
 
-  #users .uzr.superUser {
+  #users .uzr.baqer {
     box-shadow: inset 0 0 0 rgba(0, 0, 0, .08), 0 0 3px #000;
     margin-bottom: 3px !important;
     margin-top: 2px !important;
-    border-radius: 0px 0px 0px 0px;
+    border-radius: 0px;
     border: 1px solid #fff;
     background-image: url(https://up6.cc/2024/11/173276947009741.jpg);
     background-size: 100%;
   }
 
-  #users .uzr.superUser .d-flex.fl {
+  #users .uzr.baqer .d-flex.fl {
     padding-right: 0px !important;
   }
 
-  #users .uzr.superUser .u-topic {
+  #users .uzr.baqer .u-topic {
     margin-left: 5px;
     -webkit-background-clip: text;
     background-image: url(https://up6.cc/2024/11/173273771785081.png);
     background-size: cover;
   }
 
-  #users .uzr.superUser .itarr_superUser {
+  #users .uzr.baqer .itarr_baqer {
     width: 80px;
     height: 80px;
     margin-top: -39px;
@@ -92,9 +83,9 @@ function addCustomStyles() {
 `);
 
 const xxxx = [{
-  'name': "superUser",  // تم تغيير الاسم هنا
+  'name': "baqer",
   'deco': "‏NEW",
-  'cls': "superUser",  // تم تغيير الكلاس إلى superUser
+  'cls': "baqer",
   'icon': "https://www2.0zz0.com/2024/12/13/18/278696375.gif"
 }];
 
@@ -120,7 +111,7 @@ setInterval(() => {
 }, 2000);
 
 const seo = [{
-  name: "NEW",  // تم تغيير الاسم هنا أيضًا
+  name: "‏NEW",
   icon: "https://up6.cc/2024/12/173419907167791.png"
 }];
 
