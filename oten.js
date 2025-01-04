3e0fb4771b32a1b3a64e603899eb89cdfee6bea8
Nedblack <108587461+Nedblack@users.noreply.github.com>
2025-01-04 06:51:58 +0300

    Update oten.js

--- oten.js
+++ oten.js
@@ -1,3 +1,1 @@
-$("<div class='fr borderg' style='display: none;'><a href='https://iraqichat.co/'>دردشة حلوات العراق</a></div>").insertAfter($(".loginstat"))
-$("<div class='fr borderg' style='display: none;'><a href='https://iraqichat.co/'>شات حلوات العراق</a></div>").insertAfter($(".loginstat"))
 
